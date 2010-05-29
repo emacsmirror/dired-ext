74f06a7562b981a92b8edbc919ac74b7a2b6667e
Joe Bloggs <vapniks@yahoo.com>
2010-05-29 20:27:09 +0000

    Updated comment of dired-find-duplicates to reflect new functionality
    git-svn-id: https://joebloggs.svn.cvsdude.com/odesk/dired@113 f1a581bd-6a14-4726-baf4-9ff2dff07489

--- dired-ext.el
+++ dired-ext.el
@@ -25,11 +25,22 @@
 ;;; Code:
 (eval-when-compile (require 'cl))
 (defun* dired-find-duplicates (files dir)
-  "Find the duplicated files and put them in a dired buffer.
+  "Find duplicates of files and put them in a dired buffer.
 FILES is a list of files which will be compared. DIR is the directory
 which will be checked for duplicates of any of the files in the list.
 Any matching files will be placed in a new dired buffer with name
-*duplicated files*."
+*duplicated files*.
+When called interactively from a dired buffer, the marked files in that dired buffer will
+be treated as the orginals whose duplicates are to be found, and the user will be prompted
+for a directory to search for duplicates.
+If the function is called with 1 prefix arg then the original files that have duplicates
+will be marked for deletion.
+With 2 prefix args the files in the *duplicate files* buffer will be marked for deletion.
+With 3 prefix args the original files will be placed in the *duplicated files* buffer,
+interleaved with the duplicates (original file first, followed by duplicates), and 
+the original files will be marked for deletion.
+With 4 prefix args the behaviour is the same as with 3 prefix args except that the 
+duplicate files will be marked for deletion instead of the originals."
   (interactive (list (dired-get-marked-files)
 		     (read-directory-name "Directory to be checked: ")))
 
